944f5f16c738213b8c956826e1a8fa527c7013ff
Alexis Cowles <alexis.cowles@patelhs.org>
2020-10-08 14:42:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    //This project is about creating your own character
+System.out.println("Welcome to your new world!");
+System.out.print("Today you will create your own avatar");//Dialogue to introduce the person
+
+System.out.println(", but keep in mind that you only have a certain amount of money.");
+
+System.out.println("Choices to customize your characters body type will not cost money");
+
+double credits = 2000.00; //amount of money they can spend
+
+System.out.println("Total money = $" + 2000.00);
+
+
+
+  }
+}
